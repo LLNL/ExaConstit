2d94919cd77f60ec2a820a5e3be879f04c29feae
rcarson3 <rac428@cornell.edu>
2019-08-13 10:15:12 -0700

    Remove MFEM related CMake Files Conduit was previously making use of the MFEM FindConduit cmake file in order to just get things to work. I've now removed that and rewritten the cmake file to work with both the spack installed conduit and regular conduit install.
    I also added a gitignore file for the entire project, so random files aren't committed by accident.

--- cmake/thirdpartylibraries/FindConduit.cmake
+++ cmake/thirdpartylibraries/FindConduit.cmake
@@ -1,51 +1,77 @@
-# Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at the
-# Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights reserved.
-# See file COPYRIGHT for details.
-#
-# This file is part of the MFEM library. For more information and source code
-# availability see http://mfem.org.
-#
-# MFEM is free software; you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License (as published by the Free
-# Software Foundation) version 2.1 dated February 1999.
-
 # Defines the following variables:
 #   - CONDUIT_FOUND
 #   - CONDUIT_LIBRARIES
 #   - CONDUIT_INCLUDE_DIRS
 
-# check to see if relay requires hdf5, if so make sure to set HDF5
-# as a required dep
-if(EXISTS ${CONDUIT_DIR}/include/conduit/conduit_relay_hdf5.hpp)
-  message(STATUS "Conduit Relay HDF5 Support is ENABLED")
-  # we only need  HDF5 if Conduit was built with HDF5 support
-  set(Conduit_REQUIRED_PACKAGES "HDF5" CACHE STRING
-      "Additional packages required by Conduit.")
+#If find_package doesn't find CONDUIT it will overwrite CONDUIT_DIR which we will need for alternative ways of finding the package
+set(CONDUIT_DIR_TEMP ${CONDUIT_DIR})
+#If this was installed using SPACK this will fail and so we have to manually find things.
+find_package(CONDUIT
+             NO_DEFAULT_PATH
+             PATHS ${CONDUIT_DIR}/lib/cmake)
+
+if(CONDUIT_FOUND)
+
+  set(CONDUIT_LIBRARIES ${conduit::conduit})
+
+
+  if(CONDUIT_RELAY_HDF5_ENABLED)
+     set(HDF5_ROOT ${CONDUIT_HDF5_DIR})
+     find_package(HDF5 REQUIRED)
+     message("HDF5 Libraries found: ${HDF5_LIBRARIES}")
   else()
-    message(STATUS "Conduit Relay HDF5 Support is DISABLED")
+     set(HDF5_LIBRARIES "")
+  endif()
+
+else()
+  #Find package appears to be erasing this variable if it isn't found...
+  set(CONDUIT_DIR ${CONDUIT_DIR_TEMP} CACHE PATH "" FORCE)
+
+  find_path( CONDUIT_INCLUDE_DIRS NAMES conduit.h
+             PATHS  ${CONDUIT_DIR}/include/ ${CONDUIT_DIR}/include/conduit/
+             NO_DEFAULT_PATH
+             NO_CMAKE_ENVIRONMENT_PATH
+             NO_CMAKE_PATH
+             NO_SYSTEM_ENVIRONMENT_PATH
+             NO_CMAKE_SYSTEM_PATH)
+  #We have three different libraries that we need to find
+
+  find_library( CONDUIT_LIBRARY NAMES conduit
+                PATHS ${CONDUIT_DIR}/lib/
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                                NO_CMAKE_SYSTEM_PATH)
+
+  find_library( CONDUIT_RELAY_LIBRARY NAMES relay conduit_relay
+                PATHS ${CONDUIT_DIR}/lib/
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH)
+
+  find_library( CONDUIT_BLUEPRINT_LIBRARY NAMES blueprint conduit_blueprint
+                PATHS ${CONDUIT_DIR}/lib/
+                NO_DEFAULT_PATH
+                NO_CMAKE_ENVIRONMENT_PATH
+                NO_CMAKE_PATH
+                NO_SYSTEM_ENVIRONMENT_PATH
+                NO_CMAKE_SYSTEM_PATH)
+
+  set(CONDUIT_LIBRARIES ${CONDUIT_LIBRARY} ${CONDUIT_RELAY_LIBRARY} ${CONDUIT_BLUEPRINT_LIBRARY})
+  #We need to double check and see if Conduit was built with HDF5 support and if so we need to find the passed in
+  #HDF5 directory or else we'll most likely just use the built in system version which might cause issues
+  if(EXISTS ${CONDUIT_DIR}/include/conduit/conduit_relay_hdf5.hpp)
+    message(STATUS "Conduit Relay HDF5 Support is ENABLED")
+    find_package(HDF5 REQUIRED)
+  else()
+    set(HDF5_LIBRARIES "")
+  endif()
+
 endif()
 
-include(MfemCmakeUtilities)
-mfem_find_package(Conduit CONDUIT CONDUIT_DIR
-  "include;include/conduit" conduit.hpp "lib" conduit
-  "Paths to headers required by Conduit." "Libraries required by Conduit."
-  ADD_COMPONENT relay
-    "include;include/conduit" conduit_relay.hpp "lib" conduit_relay
-  ADD_COMPONENT blueprint
-      "include;include/conduit" conduit_blueprint.hpp "lib" conduit_blueprint)
-
-      find_library( CONDUIT_RELAY_LIBRARY NAMES relay conduit_relay
-              PATHS ${CONDUIT_DIR}/lib/
-              NO_DEFAULT_PATH
-              NO_CMAKE_ENVIRONMENT_PATH
-              NO_CMAKE_PATH
-              NO_SYSTEM_ENVIRONMENT_PATH
-              NO_CMAKE_SYSTEM_PATH)
-
-find_library( CONDUIT_BLUEPRINT_LIBRARY NAMES blueprint conduit_blueprint
-              PATHS ${CONDUIT_DIR}/lib/
-              NO_DEFAULT_PATH
-              NO_CMAKE_ENVIRONMENT_PATH
-              NO_CMAKE_PATH
-              NO_SYSTEM_ENVIRONMENT_PATH
-              NO_CMAKE_SYSTEM_PATH)
+find_package_handle_standard_args(CONDUIT  DEFAULT_MSG
+                                  CONDUIT_INCLUDE_DIRS
+                                  CONDUIT_LIBRARIES )
