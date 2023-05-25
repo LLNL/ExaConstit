4cde8b58d201a9e599d8fdd75f79148ca32b0a4e
Robert Carson <carson16@llnl.gov>
2023-05-25 13:41:17 -0700

    Update flux map based on extensive testing on an LC CTS machine

--- workflows/optimization/flux_map.py
+++ workflows/optimization/flux_map.py
@@ -1,6 +1,6 @@
 import os
 import os.path
-import subprocess
+import glob
 import sys
 import re
 import numpy as np
@@ -9,14 +9,21 @@ import flux
 import flux.job
 import flux.job.stats
 import flux.job.info
+import flux.resource
 
 from ExaConstit_Logger import write_ExaProb_log
+from ExaConstit_Problems import cd
 
 
 def map_custom(problem, igeneration, genes):    
     status = []
     f_objective = []
     flux_handle = flux.Flux()
+
+    machine_name = os.uname().nodename
+    spectrum_machine = False
+    if ("lassen" in machine_name or "sierra" in machine_name or "ansel" in machine_name or "summit" in machine_name or "andes" in machine_name):
+        spectrum_machine = True
 
     # Preprocess all of the genes first
     for igene, gene in enumerate(genes):
@@ -25,42 +32,31 @@ def map_custom(problem, igeneration, genes):
     # Submit all of the flux jobs
     jobids = []
     jobnames = []
-    for igene, gene in enumerate(genes):
-        istatus = []
-        for iobj in range(problem.n_obj):
+    for iobj in range(problem.n_obj - 1, -1, -1):
+        for igene, gene in enumerate(genes):
             gene_dir = "gen_" + str(igeneration)
             fdir = os.path.join(problem.workflow_dir, gene_dir, "")
             rve_name = "gene_" + str(igene) + "_obj_" + str(iobj)
             fdironl = os.path.join(fdir, rve_name, "")
 
-            flux_obj = problem.job_script
-            fh_output = os.path.join(fdironl, "optimization_out.txt")
-            fh_error  = os.path.join(fdironl, "optimization_err.txt")
-            modifiers = {"binary":problem.bin_mechanics, "nnodes":problem.nnodes, "ntasks":problem.ncpus, "ngpus":problem.ngpus, "output_name":fh_output, "error_name":fh_error, "timeout":int(problem.timeout[iobj])}
-            for iheader, repl_val in modifiers.items():
-                search = "%%" + iheader + "%%"
-                flux_obj = re.sub(search, str(repl_val), flux_obj)
-            # Output toml file
-            fh = os.path.join(fdironl, os.path.basename("mechanics.flux"))
-            # Check to see if it is a symlink and if so remove the link
-            if os.path.islink(fh):
-                os.unlink(fh)
-            # We can now safely write out the file
-            with open(fh, "w") as f:
-                f.write(flux_obj)
-            print(fh)
-            jobspec = flux.job.JobspecV1.from_nest_command(
-                ["python3", fh],
-                num_slots=problem.nnodes,
-                cores_per_slot=problem.ncpus,
-                gpus_per_slot=problem.ngpus,
-                num_nodes=problem.nnodes,
+            ngpus = None
+            if problem.ngpus[iobj] > 0:
+                ngpus = 1
+
+            jobspec = flux.job.JobspecV1.from_command(
+                [problem.bin_mechanics, "-opt", "options.toml"],
+                num_nodes=problem.nnodes[iobj],
+                num_tasks=problem.ncpus[iobj],
+                cores_per_task=1,
+                gpus_per_task=ngpus
             )
+
             jobspec.cwd = fdironl
             jobspec.stdout = "flux_output.txt"
             jobspec.stderr = "flux_error.txt"
             jobspec.environment = dict(os.environ)
-            jobspec.setattr_shell_option("mpi", "spectrum")
+            if spectrum_machine:
+                jobspec.setattr_shell_option("mpi", "spectrum")
             jobspec.setattr_shell_option("gpu-affinity", "per-task")
             jobspec.setattr_shell_option("cpu-affinity", "per-task")
             jobspec.duration = int(problem.timeout[iobj])
@@ -70,22 +66,39 @@ def map_custom(problem, igeneration, genes):
     # Wait on all of our flux jobs to finish
     stats = flux.job.stats.JobStats(flux_handle)
     for j in jobids:
+        write_ExaProb_log(f"[running: {stats.run}, pending: {stats.pending}, run/complete: {stats.running}]", "info")
         jobid, istatus, errnum = flux.job.wait(flux_handle, j)
+        stats.update_sync()
         if not istatus:
             status.append(errnum)
         else:
             status.append(0)
+
+    # Summit jobs might produce spurious core dumps at end of simulation
+    # due to some weird issues regarding flux's MPI library finalize function
+    # We don't have this issue when just using jsruns instead...
+    # Nonetheless, we want to delete these to save disk space
+    for igene, gene in enumerate(genes):
+        for iobj in range(problem.n_obj):
+            gene_dir = "gen_" + str(igeneration)
+            fdir = os.path.join(problem.workflow_dir, gene_dir, "")
+            rve_name = "gene_" + str(igene) + "_obj_" + str(iobj)
+            fdironl = os.path.join(fdir, rve_name, "")
+            with cd(fdironl):
+                for f in glob.glob("core*"):
+                    os.remove(f)
     # Get info about the job like runtime, status, and errnum
     jobs = flux.job.JobList(flux_handle, ids=jobids).jobs()
     for job, name in zip(jobs, jobnames):
         write_ExaProb_log(f"RVE name: {name}, flux id: {job.id}, runtime: {job.runtime:12.3f}s, status: {job.status}, errnum: {job.returncode}", "info")
     # Convert status back into a 2d array
     status = [
-        status[i : i + problem.n_obj] for i in range(0, len(status), problem.n_obj)
+        status[i : i + len(genes)] for i in range(0, len(status), len(genes))
     ]
     # Post-process all of the data last
     for igene, gene in enumerate(genes):
-        f = problem.postprocess(igeneration, igene, status[igene])
+        stats = [status[i][igene] for i in range(problem.n_obj)]
+        f = problem.postprocess(igeneration, igene, stats)
         f_objective.append(np.copy(f))
 
     return f_objective
@@ -94,15 +107,18 @@ def map_custom(problem, igeneration, genes):
 # Will want a custom way to handle one off launches for failed tests
 def map_custom_fail(problem, igeneration, gene, igene):
     status = []
-    f_objective = []
     flux_handle = flux.Flux()
 
     # Preprocess all of the genes first
-    problem.preprocess(gene, igeneration, igene)
+    problem.preprocess(gene, igeneration, igene, True)
 
+    machine_name = os.uname().nodename
+    spectrum_machine = False
+    if ("lassen" in machine_name or "sierra" in machine_name or "ansel" in machine_name or "summit" in machine_name or "andes" in machine_name):
+        spectrum_machine = True
     # Submit all of the flux jobs
     jobids = []
-
+    jobnames = []
     istatus = []
     for iobj in range(problem.n_obj):
         gene_dir = "gen_" + str(igeneration)
@@ -110,42 +126,53 @@ def map_custom_fail(problem, igeneration, gene, igene):
         rve_name = "gene_" + str(igene) + "_obj_" + str(iobj)
         fdironl = os.path.join(fdir, rve_name, "")
 
-        flux_obj = problem.job_script
-        fh_output = os.path.join(fdironl, "optimization_out.txt")
-        fh_error  = os.path.join(fdironl, "optimization_err.txt")
-        modifiers = {"binary":problem.bin_mechanics, "nnodes":problem.nnodes, "nntasks":problem.ncpus, "ngpus":problem.ngpus,
-        "output_name":fh_output, "error_name":fh_error}
-        for iheader, repl_val in modifiers.items():
-            search = "%%" + iheader + "%%"
-            flux_obj = re.sub(search, str(repl_val), flux_obj)
-        # Output toml file
-        fh = os.path.join(fdironl, os.path.basename("mechanics.flux"))
-        # Check to see if it is a symlink and if so remove the link
-        if os.path.islink(fh):
-            os.unlink(fh)
-        # We can now safely write out the file
-        with open(fh, "w") as f:
-            f.write(flux_obj)
+        ngpus = None
+        if problem.ngpus[iobj] > 0:
+            ngpus = 1
 
-        jobspec = flux.job.JobspecV1.from_nest_command(
-            ["python3", fh],
-            num_slots=problem.nnodes,
-            cores_per_slot=problem.ncpus,
-            num_nodes=problem.nnodes,
+        jobspec = flux.job.JobspecV1.from_command(
+            [problem.bin_mechanics, "-opt", "options.toml"],
+            num_nodes=problem.nnodes[iobj],
+            num_tasks=problem.ncpus[iobj],
+            cores_per_task=1,
+            gpus_per_task=ngpus
         )
 
         jobspec.cwd = fdironl
-        jobspec.stdout = fh_output
-        jobspec.stderr = fh_error
+        jobspec.stdout = "flux_output.txt"
+        jobspec.stderr = "flux_error.txt"
+        jobspec.environment = dict(os.environ)
+        if spectrum_machine:
+            jobspec.setattr_shell_option("mpi", "spectrum")
+        jobspec.setattr_shell_option("gpu-affinity", "per-task")
+        jobspec.setattr_shell_option("cpu-affinity", "per-task")
+        jobspec.duration = int(problem.timeout[iobj])
         jobids.append(flux.job.submit(flux_handle, jobspec, waitable=True))
+        jobnames.append(rve_name)
 
     # Wait on all of our flux jobs to finish
+    stats = flux.job.stats.JobStats(flux_handle)
     for j in jobids:
+        write_ExaProb_log(f"[running: {stats.run}, pending: {stats.pending}, run/complete: {stats.running}]", "info")
         jobid, istatus, errnum = flux.job.wait(flux_handle, j)
+        stats.update_sync()
         if not istatus:
             status.append(errnum)
         else:
             status.append(0)
+
+    # Summit jobs might produce spurious core dumps at end of simulation
+    # due to some weird issues regarding flux's MPI library finalize function
+    # We don't have this issue when just using jsruns instead...
+    # Nonetheless, we want to delete these to save disk space
+    for iobj in range(problem.n_obj):
+        gene_dir = "gen_" + str(igeneration)
+        fdir = os.path.join(problem.workflow_dir, gene_dir, "")
+        rve_name = "gene_" + str(igene) + "_obj_" + str(iobj)
+        fdironl = os.path.join(fdir, rve_name, "")
+        with cd(fdironl):
+            for f in glob.glob("core*"):
+                os.remove(f)
 
     # Convert status back into a 2d array
     status = [
@@ -153,7 +180,7 @@ def map_custom_fail(problem, igeneration, gene, igene):
     ]
 
     # Post-process all of the data last
-    f = problem.postprocess(igeneration, igene, status[0])
-    f_objective.append(np.copy(f))
+    f = problem.postprocess(igeneration, igene, status[0], True)
+    f_objective = np.copy(f)
 
     return f_objective
