7313a1eb84e22e3698668b83d3b167a811a9c55a
Robert Carson <carson16@llnl.gov>
2023-08-17 16:19:09 -0700

    Finally fix broken test suite on macs and windows...

--- test/test_mechanics.py
+++ test/test_mechanics.py
@@ -6,6 +6,7 @@ import os
 import multiprocessing
 import numpy as np
 import unittest
+from sys import platform
 
 def check_stress(ans_pwd, test_pwd, test_case):
     answers = []
@@ -68,8 +69,10 @@ def run():
     # We divide by 2 since we use 2 cores per MPI call
     # However, this command only works on Unix machines since Windows
     # hasn't added support for this command yet...
-    num_processes = int(len(os.sched_getaffinity(0)) / 2)
-    # num_processes = int(multiprocessing.cpu_count() / 2)
+    if platform == "linux" or platform == "linux2":
+        num_processes = int(len(os.sched_getaffinity(0)) / 2)
+    else:
+        num_processes = int(multiprocessing.cpu_count() / 2)
     print(num_processes)
     pool = multiprocessing.Pool(num_processes)
     pool.map(runSystemCommands, params)
@@ -132,8 +135,10 @@ def runExtra():
     # We divide by 2 since we use 2 cores per MPI call
     # However, this command only works on Unix machines since Windows
     # hasn't added support for this command yet...
-    num_processes = int(len(os.sched_getaffinity(0)) / 2)
-    # num_processes = multiprocessing.cpu_count() / 2
+    if platform == "linux" or platform == "linux2":
+        num_processes = int(len(os.sched_getaffinity(0)) / 2)
+    else:
+        num_processes = int(multiprocessing.cpu_count() / 2)
     print(num_processes)
     pool = multiprocessing.Pool(num_processes)
     pool.map(runExtraSystemCommands, params)
