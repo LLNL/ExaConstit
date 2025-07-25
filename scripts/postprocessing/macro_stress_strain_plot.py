8c87f8173323deb6b73a95b73b9248158f9501ca
rcarson3 <robert.carson01@gmail.com>
2025-07-24 21:09:16 -0700

    Update plotting script to work with new format

--- scripts/postprocessing/macro_stress_strain_plot.py
+++ scripts/postprocessing/macro_stress_strain_plot.py
@@ -23,42 +23,50 @@ mrks = ['*', ':', '--', 'solid']
 
 fig, ax = plt.subplots(1)
 
-#number of time steps
 nsteps = 40
-
 # uncomment the below when the fileLoc is valid
 #data = np.loadtxt(fileLoc+'avg_stress.txt', comments='%')
 # only here to have something that'll plot
-data = np.ones((nsteps, 6))
+data = np.ones((nsteps, 8))
+# First two columns are time and volume
+# Next 6 columns are the Cauchy stress in voigt notation
+sig = data[:,4]
+vol = data[:,1]
+time = data[:,0]
+nsteps = data.shape[0] + 1
+
+sig = np.r_[0, sig]
+vol = np.r_[1, vol]
+time = np.r_[0, time]
+
+# If setup for it you can also request either the eulerian strain or deformation gradient
+# in which case most of the below can be ignored
 
 epsdot = 1e-3
-
-
-sig = data[:,2]
-# uncomment the below when the fileLoc is valid
-#time = np.loadtxt(fileLoc+'custom_dt.txt')
 # only here to have something that'll plot
-time = np.ones(nsteps)
-
 eps = np.zeros(nsteps)
 
 for i in range(0, nsteps):
-    dtime = time[i]
+    if (i == 0):
+        dtime = time[i]
+    else:
+        dtime = time[i] - time[i - 1]
+    # Stress is not always monotonically increasing so this is not always the
+    # best assumption
     if sig[i] - sig[i - 1] >= 0:
         eps[i] = eps[i - 1] + epsdot * dtime
     else:
         eps[i] = eps[i - 1] - epsdot * dtime
 
-ax.plot(eps, sig, 'r')
-
+# For true strain the np.log(1 + eps) will provide the correct conversion
+ax.plot(np.log(1.0 + eps), sig, 'r')
 ax.grid()
 
-# change this to fit your data                 
+# change this to fit your data
 # ax.axis([0, 0.01, 0, 0.3])
 
-ax.set_ylabel('Macroscopic engineering stress [GPa]')
-ax.set_xlabel('Macroscopic engineering strain [-]')
+ax.set_ylabel('Macroscopic true stress [GPa]')
+ax.set_xlabel('Macroscopic true strain [-]')
 
-plt.close()
 fig.show()
 plt.show()
